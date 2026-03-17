fbbb8748239ed53bce20aad370b64a6540df4275
Sergey Belkin <sevbelkin@mai.education>
2026-03-17 12:22:59 +0300

    add_adapters_logic_endpoints

--- tax_gateway/views.py
+++ tax_gateway/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .adapters import get_adapter
+import uuid
 
-#ДАНЯ ЭТО ЗАГЛУШКА СЕРИАЛИЗАТОРА
-class MockSerializer:
-    def __init__(self, data):
-        self.data = data
-        self.is_valid = True
 
 class TaxReportView(APIView):
     """ Эндпоинт для подачи отчета: POST /api/v1/tax/report """
@@ -16,54 +13,39 @@ class TaxReportView(APIView):
         country = request.query_params.get('country')
 
         if not country:
-            return Response({
-                "error_code": "VALIDATION_ERROR",
-                "message": "Параметр 'country' обязателен",
-                "details": {}
-            }, status=status.HTTP_400_BAD_REQUEST)
-        
-        #Вызов зашлушки вместо реального сериализатора
-        serializer = MockSerializer(data=request.data)
-        
-        if serializer.is_valid:
-            return Response({
-                "status": "accepted",
-                "report_id": "mock_id_12345",
-                "timestamp": "2026-03-16T17:20:00Z"
-            }, status=status.HTTP_201_CREATED)
+            return Response({"error": "Country required"}, status=400)
+
+        adapter = get_adapter(country)
+        result = adapter.send_report(request.data)
+
+        return Response({
+            "status": "accepted",
+            "details": result,
+            "report_id": str(uuid.uuid4())
+        }, status=status.HTTP_201_CREATED)
 
 class TaxStatusView(APIView):
     """ Проверка статуса: GET /api/v1/tax/status/{id} """
     def get(self, request, report_id):
-        return Response({
-            "report_id": report_id,
-            "status": "in_progress"
-        }, status=status.HTTP_200_OK)
+        country = request.query_params.get('country', 'russia') # в будущем нужно будт брать страну из БД по id, а не из query параметров
+
+        adapter = get_adapter(country)
+        result = adapter.get_status(report_id)
+
+        return Response(result, status=status.HTTP_200_OK)
     
 class TaxValidateView(APIView):
     """ Эндпоинт для предварительной проверки данных: POST /api/v1/tax/validate """
-
     def post(self, request):
         country = request.query_params.get('country')
-
         if not country:
             return Response({
                 "error_code": "VALIDATION_ERROR",
-                "message": "Параметр 'country' обязателен",
-                "details": {}
+                "message": "Параметр 'country' обязателен"
             }, status=status.HTTP_400_BAD_REQUEST)
 
-        #Вызов зашлушки вместо реального сериализатора
-        serializer = MockSerializer(data=request.data)
-
-        if serializer.is_valid:
-            return Response({
-                "is_valid": True,
-                "message": f"Данные корректны для юрисдикции: {country}",
-                "errors": []
-            }, status=status.HTTP_200_OK)
+        # Вызываем адаптер для валидации
+        adapter = get_adapter(country)
+        validation_result = adapter.validate(request.data)
         
-        return Response({
-            "is_valid": False,
-            "errors": []
-        }, status=status.HTTP_200_OK)
+        return Response(validation_result, status=status.HTTP_200_OK)
